e205234e3ccbf7be69699edbcbee03a3d505055a
Guadalupe Murgo <guadazoemurgo@gmail.com>
2025-12-23 11:04:07 -0300

    control hub image added

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,34 @@
-import './App.css'
+import React, { useState, useEffect } from 'react';
+import { Stage, Layer, Image } from 'react-konva';
+import ControlHubIMG from './assets/IMG/ControlHub.png';
 
 function App() {
+  const [img, setImg] = useState(null);
+
+  useEffect(() => {
+    const image = new window.Image();
+    image.src = ControlHubIMG;
+    image.onload = () => setImg(image);
+  }, []);
+
   return (
-    <h1>Wiresketch</h1>
-  )
+    <div style={{ width: '100vw', height: '100vh' }}>
+      <Stage width={window.innerWidth} height={window.innerHeight}>
+        <Layer>
+          {img && (
+            <Image
+              image={img}
+              x={50}
+              y={50}
+              width={(img.width / img.height) * 200}
+              height={200}
+              draggable
+            />
+          )}
+        </Layer>
+      </Stage>
+    </div>
+  );
 }
 
-export default App
+export default App;
